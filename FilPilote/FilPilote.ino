a9378894e9a888c596e6d19b3fbff8b0c710ca7b
h <h>
2016-10-07 18:54:21 +0200

    tes

--- FilPilote/FilPilote.ino
+++ FilPilote/FilPilote.ino
@@ -2,7 +2,7 @@
 
 
 #include "DecodeHomeEasy.h"
-DecodeHomeEasy HEasy ;
+DecodeHomeEasy HEasy(1) ;
 
 #include "fifo.h"
 TFifo  fifo;
@@ -40,9 +40,10 @@ byte        pinlearning ;
 //le signal du RFM69 entre sur int ext    d3 : int1
 
 #define VERSION 1
-
+#define EMPTY_ADDR 0xff 
+static unsigned long  last;
 void ext_int_1(void) {
-    static unsigned long  last;
+    
     word   pulse ;
 
     pinData=digitalRead(PDATA);
@@ -53,18 +54,27 @@ void ext_int_1(void) {
     fifo.put(pulse);
 }
 
+void setStatusLed()
+{
+    if ( (gid[0]!=EMPTY_ADDR)&&(gid[1]!=EMPTY_ADDR)&&(gid[2]!=EMPTY_ADDR)&&(gid[3]!=EMPTY_ADDR) )
+        digitalWrite(PINSTATUS,HIGH);
+    else
+        digitalWrite(PINSTATUS,LOW );
+
+}
 void setup () {
 	  LastReceive = 0 ;
+    last=0;
     
-    Serial.begin(38400);
-//    Serial.begin(115200);
+//    Serial.begin(38400);
+    Serial.begin(115200);
     
    // initialize the LED pin as an output:
     pinMode(ledPin, OUTPUT);       
-    pinMode(PDATA, INPUT);
+    pinMode(PDATA, INPUT_PULLUP);
 
 
-    pinMode(PIN1, OUTPUT);       
+/*    pinMode(PIN1, OUTPUT);       
     pinMode(PIN2, OUTPUT);       
     pinMode(PIN3, OUTPUT);       
     pinMode(PIN4, OUTPUT);       
@@ -72,8 +82,8 @@ void setup () {
     pinMode(PIN6, OUTPUT);       
     pinMode(PIN7, OUTPUT);       
     pinMode(PIN8, OUTPUT);       
-
-    pinMode(PINLEARNING, INPUT );       
+*/
+    pinMode(PINLEARNING, INPUT_PULLUP );       
     pinMode(PINSTATUS  , OUTPUT);       
    
     for (byte i=0;i<4;i++)gid[i]=0;
@@ -81,13 +91,10 @@ void setup () {
     for (byte i=0;i<4;i++)
         EEPROM.get(i,gid[i]);
 
-    if ( (gid[0]!=0)&&(gid[1]!=0)&&(gid[2]!=0)&&(gid[3]!=0) )
-        digitalWrite(PINSTATUS,HIGH);
-    else
-        digitalWrite(PINSTATUS,LOW );
+    setStatusLed();
 
     learningInProgress=0;
-    LearningTime=0;
+    LearningTime=-30000;
 
     attachInterrupt(1, ext_int_1, CHANGE);
 
@@ -95,7 +102,7 @@ void setup () {
     Serial.print("Version ");
     Serial.println(VERSION);
     Serial.print( "Adress: " ) ; 
-   
+    for (byte i=0;i<4;i++) Serial.print(gid[i],HEX);Serial.println();
 }
 
 void PulseLed()
@@ -120,7 +127,9 @@ byte id[4],unitcode,cmnd    ;
     if (Dt==1){
     }    */
     if (p != 0) {
-
+        if (HEasy.total_bits>1){
+          Serial.print(HEasy.total_bits,HEX);Serial.println();
+        }
         if (HEasy.nextPulse(p,pinData )) {
 
 	        id[0]        = HEasy.data[5];            /* id emetteur 0..3  */
@@ -153,18 +162,21 @@ byte id[4],unitcode,cmnd    ;
                     EEPROM.put(i,gid[i]);
 
                 Serial.print( "Learn :" ) ; for (byte i=5;i<9;i++) Serial.print(HEasy.data[i],HEX);Serial.println();
+                setStatusLed();
             }
 
-        PulseLed();
-        HEasy.resetDecoder();
+//        PulseLed();
+          HEasy.resetDecoder();
       	}
     }
     //learning button pressed
     pinlearning = digitalRead(PINLEARNING) ;
     if ((learningInProgress==0)&&(pinlearning==0)){
-        LearningTime=millis();
-        learningInProgress=1;
-        Serial.println( "Learning in progress" ) ; 
+        if ((millis()-LearningTime) >30000 ){
+          LearningTime=millis();
+          learningInProgress=1;
+          Serial.println( "Learning in progress" ) ; 
+        }
     }
     //Blink status led during learning
     if (learningInProgress==1){
@@ -179,19 +191,21 @@ byte id[4],unitcode,cmnd    ;
         if ((millis()-LearningTime) >30000 ){
             learningInProgress=0 ;
             Serial.println( "Learning timout" ) ; 
+            setStatusLed();
+            
         }
 
         //clear adress if learning pressed during 3 seconds 
         if ( (pinlearning==0) &&  ((millis()-LearningTime) >3000 )  ){
-            gid[0]        =  0 ;            /* id emetteur 0..3  */
-            gid[1]        =  0 ;            /* id emetteur 0..FF */
-            gid[2]        =  0 ;            /* id emetteur 0..FF */
-            gid[3]        =  0;             /* id emetteur 0..FF */
+            gid[0]        =  EMPTY_ADDR ;            /* id emetteur 0..3  */
+            gid[1]        =  EMPTY_ADDR ;            /* id emetteur 0..FF */
+            gid[2]        =  EMPTY_ADDR ;            /* id emetteur 0..FF */
+            gid[3]        =  EMPTY_ADDR ;             /* id emetteur 0..FF */
             learningInProgress=0;
             for (byte i=0;i<4;i++)
                 EEPROM.put(i,gid[i]);
-            digitalWrite(PINSTATUS,HIGH);
             Serial.println( "Learning reset" ) ; 
+            setStatusLed();
 
         }
 
