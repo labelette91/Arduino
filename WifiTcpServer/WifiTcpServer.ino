78e9bb7eee1d451bbdd7b5c37b41d2b727db736d
h <h>
2016-10-16 12:14:20 +0200

    v2

--- WifiTcpServer/WifiTcpServer.ino
+++ WifiTcpServer/WifiTcpServer.ino
@@ -1,48 +1,92 @@
+/* 
+  WiFiTelnetToSerial - Example Transparent UART to Telnet Server for esp8266
 
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WiFi library for Arduino environment.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 
-// Hardcode WiFi parameters as this isn't going to be moving around.
+//how many clients should be able to telnet to this ESP8266
+#define MAX_SRV_CLIENTS 3
 const char* ssid = "Livebox-BC43";
 const char* password = "C03294468E618A316C6759A245";
 
-// Start a TCP Server on port 5045
-WiFiServer server(5045);
-WiFiClient client;
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
 void setup() {
   Serial.begin(74880);
-  WiFi.begin(ssid,password);
-  Serial.println("");
-  //Wait for connection
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  delay(500);
+  WiFi.begin(ssid, password);
+  Serial.print("\nConnecting to "); Serial.println(ssid);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
+  if(i == 21){
+    Serial.print("Could not connect to"); Serial.println(ssid);
+    while(1) delay(500);
   }
-  Serial.print("Connected to "); Serial.println(ssid);
-  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
-  
-  // Start the TCP server
+  //start UART and the server
+  Serial.begin(74880);
   server.begin();
+  server.setNoDelay(true);
+  
+  Serial.print("Ready! Use 'telnet ");
+  Serial.print(WiFi.localIP());
+  Serial.println(" 23' to connect");
 }
 
-
-void TCPServer () {
-    client = server.available();
-  if (client) {
-    if (client.connected()) {
-      Serial.println("Connected to client");    
+void loop() {
+  uint8_t i;
+  //check if there are any new clients
+  if (server.hasClient()){
+    for(i = 0; i < MAX_SRV_CLIENTS; i++){
+      //find free/disconnected spot
+      if (!serverClients[i] || !serverClients[i].connected()){
+        if(serverClients[i]) serverClients[i].stop();
+        serverClients[i] = server.available();
+        Serial.print("New client: "); Serial.println(i);
+        break;
+      }
     }
-    if (client.available() > 0) {
-      // Read incoming message
-      char inChar = client.read();
-      // Send back something to the clinet
-      server.write(inChar);
-      // Echo input on Serial monitor
-      Serial.write(inChar);
+    //no free/disconnected spot so reject
+    //WiFiClient serverClient = server.available();
+    //serverClient.stop();
+  }
+  //check clients for data
+  for(i = 0; i < MAX_SRV_CLIENTS; i++){
+    if (serverClients[i] && serverClients[i].connected()){
+      if(serverClients[i].available()){
+        //get data from the telnet client and push it to the UART
+        Serial.print("client: "); Serial.println(i);
+        while(serverClients[i].available()) { Serial.print(serverClients[i].read(),HEX); Serial.print(' ');};
+      }
+    }
+  }
+  //check UART for data
+  if(Serial.available()){
+    size_t len = Serial.available();
+    uint8_t sbuf[len];
+    Serial.readBytes(sbuf, len);
+    //push UART data to all connected telnet clients
+    for(i = 0; i < MAX_SRV_CLIENTS; i++){
+      if (serverClients[i] && serverClients[i].connected()){
+        serverClients[i].write(sbuf, len);
+        delay(1);
+      }
     }
   }
 }
-   void loop() {
-  TCPServer();
-}
-
